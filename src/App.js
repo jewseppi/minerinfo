7507df528db164784432d65d3f3c0b391859f561
Joseph Silverman <jewseppi@gmail.com>
2018-07-19 13:41:04 -0400

    dashboard and routing configuration

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { Route, Switch } from 'react-router';
+import Dashboard from './dashboard';
+import AppContainer from './styles/styled';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">minerinfo</h1>
-        </header>
-      </div>
-    );
-  }
-}
+const App = () => (
+  <AppContainer>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1 className="App-title">minerinfo</h1>
+      </header>
+      <Switch>
+        <Route exact path="/dashboard" component={Dashboard} />
+      </Switch>
+    </div>
+  </AppContainer>
+);
 
-export default App
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
+import { UserAgentProvider } from '@quentin-sommer/react-useragent';
+import { Provider } from 'react-redux';
+import configureStore from './store/configure-store';
 import registerServiceWorker from './registerServiceWorker';
+import theme from './styles/theme';
+import App from './App';
+import './index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = configureStore();
+const rootEL = document.getElementById('root');
+
+const app = () => (
+  <Provider store={store}>
+    <ThemeProvider theme={theme}>
+      <UserAgentProvider ua={window.navigator.userAgent}>
+        <Router>
+          <Route path="/" component={App} />
+        </Router>
+      </UserAgentProvider>
+    </ThemeProvider>
+  </Provider>
+);
+
+ReactDOM.render(
+  app(),
+  rootEL,
+);
 registerServiceWorker();
